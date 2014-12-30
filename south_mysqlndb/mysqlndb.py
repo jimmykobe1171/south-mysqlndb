a8486b30daf0949b2393a45a382cafc05b43b8ca
Jimmy <jimmy@motherapp.com>
2014-12-30 18:52:53 +0800

    project setup

---
+++ south_mysqlndb/mysqlndb.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from south.db.mysql import DatabaseOperations as BaseDatabaseOperations
+from south.logger import get_logger
+from south.utils.py3 import text_type
+from django.db.utils import DatabaseError
+
+MYSQLNDB_SPECIFIC_ERROR = ['Converted FIXED field to DYNAMIC to enable on-line ADD COLUMN']
+
+class DatabaseOperations(BaseDatabaseOperations):
+
+    def execute(self, sql, params=[], print_all_errors=True):
+        """
+        Executes the given SQL statement, with optional parameters.
+        If the instance's debug attribute is True, prints out what it executes.
+        """
+        
+        self._possibly_initialise()
+        
+        cursor = self._get_connection().cursor()
+        if self.debug:
+            print("   = %s" % sql, params)
+
+        if self.dry_run:
+            return []
+
+        get_logger().debug(text_type('execute "%s" with params "%s"' % (sql, params)))
+
+        try:
+            cursor.execute(sql, params)
+        except DatabaseError as e:
+            if print_all_errors:
+                self._print_sql_error(e, sql, params)
+            raise
+        except Exception as e:
+            if str(e) in MYSQLNDB_SPECIFIC_ERROR:
+                print('mysqlndb specific warning: %s', e)
+
+        try:
+            return cursor.fetchall()
+        except:
+            return []
